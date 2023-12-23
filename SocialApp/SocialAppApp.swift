36822a84ea862022761adb2cef94399f9676523b
Matti Kjellstadli <40216910+mattkje@users.noreply.github.com>
2023-12-23 15:23:43 +0100

    Initial Commit

---
+++ SocialApp/SocialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialAppApp.swift
+//  SocialApp
+//
+//  Created by Matti Kjellstadli on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SocialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
